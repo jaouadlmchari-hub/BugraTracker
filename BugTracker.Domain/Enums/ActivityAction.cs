951bedfd852a8be5ddacc638a53fbcaea9077ff4
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-08 00:02:49 +0100

    feat(domain): add base entity and core enums

---
+++ BugTracker.Domain/Enums/ActivityAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Enums
+{
+    public enum ActivityAction
+    {
+        Created,
+        TitleChanged,
+        DescriptionChanged,
+        StatusChanged,
+        PriorityChanged,
+        Assigned,
+        SprintChanged,
+        Commented,
+        AttachmentAdded,
+        AttachmentRemoved
+    }
+}
---
+++ BugTracker.Domain/Enums/IssueStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Enums
+{
+    public enum IssueStatus
+    {
+        Todo,
+        InProgress,
+        InReview,
+        Done
+    }
+}
