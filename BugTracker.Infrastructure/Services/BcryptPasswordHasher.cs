74fae636a86cc4e0b7a94c87065a2c581e84d5c9
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-09 03:14:02 +0100

    feat(security): add password hasher interface and BCrypt implementation

---
+++ BugTracker.Infrastructure/Services/BcryptPasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using BugTracker.Application.Interfaces.Services;
+
+namespace BugTracker.Infrastructure.Services;
+
+public class BcryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
