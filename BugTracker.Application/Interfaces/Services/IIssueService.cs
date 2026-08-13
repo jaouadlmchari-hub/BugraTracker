81b4996ad77f519b81d1204bfcd82ffea3660450
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:26 +0100

    feat(application): add IIssueService interface

---
+++ BugTracker.Application/Interfaces/Services/IIssueService.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Domain.Enums;
+using BugTracker.Application.DTOs.Issues;
+
+public interface IIssueService
+{
+    Task<IssueDto?> GetByIdAsync(Guid issueId);
+
+    Task<IEnumerable<IssueDto>> GetByProjectAsync(Guid projectId);
+
+    Task<IssueDto> CreateAsync(Guid projectId, CreateIssueDto dto);
+
+    Task<IssueDto> UpdateAsync(Guid issueId, UpdateIssueDto dto);
+
+    Task ChangeStatusAsync(Guid issueId, IssueStatus newStatus);
+
+    Task AssignAsync(Guid issueId, Guid userId);
+
+    Task MoveToSprintAsync(Guid issueId, Guid? sprintId);
+
+    Task DeleteAsync(Guid issueId);
+}
