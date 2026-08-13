b7e62d46e53bbff5cb780f321188281f6038f31f
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:25 +0100

    feat(application): add ProjectFilterDto

---
+++ BugTracker.Application/DTOs/Projects/ProjectFilterDto.cs
@@ -0,0 +1,15 @@
+﻿using BugTracker.Domain.Enums;
+
+namespace BugTracker.Application.DTOs.Projects;
+
+public class ProjectFilterDto
+{
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 10;
+
+    public string? Search { get; set; }
+
+    public ProjectStatus? Status { get; set; }
+
+}
