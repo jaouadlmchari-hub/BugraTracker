946db3f12dffa402182cf42f7ffc87078ee994a0
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:49 +0100

    feat(application): add CreateSprintDto

---
+++ BugTracker.Application/DTOs/Sprints/CreateSprintDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BugTracker.Application.DTOs.Sprints
+{
+    public class CreateSprintDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Goal { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
