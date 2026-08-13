5eff42a3d13bcaf3bffe5a8e4ac7ddfd136ee76f
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:07:49 +0100

    feat(infrastructure): update ProjectRepository implementation

--- BugTracker.Infrastructure/Persistence/Repositories/ProjectRepository.cs
+++ BugTracker.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using BugTracker.Application.Interfaces.Repositories;
+﻿using BugTracker.Application.DTOs.Projects;
+using BugTracker.Application.Interfaces.Repositories;
 using BugTracker.Domain.Entities;
 using BugTracker.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,13 @@ namespace BugTracker.Infrastructure.Persistence.Repositories
                 .FirstOrDefaultAsync(p => p.Key == key);
         }
 
+        public async Task<Project?> GetByIdWithMembersAsync(Guid projectId)
+        {
+            return await _dbSet
+                       .Include(p => p.Members)
+                       .FirstOrDefaultAsync(p => p.Id == projectId);
+        }
+
         public async Task<IEnumerable<Project>> GetByOwnerIdAsync(Guid ownerId)
         {
             return await _dbSet
@@ -32,6 +40,40 @@ namespace BugTracker.Infrastructure.Persistence.Repositories
                 .ToListAsync();
         }
 
+        public async Task<(IEnumerable<Project> Items, int TotalCount)> GetPaginatedAsync(
+               ProjectFilterDto filter,Guid userId,bool isAdmin)
+        {
+            IQueryable<Project> query = _dbSet;
+
+            if (!isAdmin)
+            {
+                query = query.Where(p =>
+                    p.Members.Any(m => m.UserId == userId));
+            }
+
+            if (!string.IsNullOrWhiteSpace(filter.Search))
+            {
+                query = query.Where(p =>
+                    p.Name.Contains(filter.Search) ||
+                    p.Key.Contains(filter.Search));
+            }
+
+            if (filter.Status.HasValue)
+            {
+                query = query.Where(p =>
+                    p.Status == filter.Status.Value);
+            }
+
+            var totalCount = await query.CountAsync();
+
+            var items = await query
+                .OrderBy(p => p.Name)
+                .Skip((filter.PageNumber - 1) * filter.PageSize)
+                .Take(filter.PageSize)
+                .ToListAsync();
+
+            return (items, totalCount);
+        }
         public async Task<bool> IsKeyUniqueAsync(string key)
         {
             return !await _dbSet
