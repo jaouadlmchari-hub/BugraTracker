dd2f02d3633b882c6aab0fdd5e1a84eb1d37d16b
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:15:30 +0100

    feat(application): add UpdateCommentValidator

---
+++ BugTracker.Application/Validators/Comments/UpdateCommentValidator.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Application.DTOs.Comments;
+using FluentValidation;
+
+
+namespace BugTracker.Application.Validators.Comments
+{
+    public class UpdateCommentValidator : AbstractValidator<UpdateCommentDto>
+    {
+        public UpdateCommentValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty()
+                .Must(content => !string.IsNullOrWhiteSpace(content))
+                .WithMessage("Le contenu du commentaire est obligatoire.");
+        }
+    }
+}
