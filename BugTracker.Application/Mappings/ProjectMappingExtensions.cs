94b193ff9d9837554714e064d6d5cd16c0420f07
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:54 +0100

    feat(application): add ProjectMappingExtensions

---
+++ BugTracker.Application/Mappings/ProjectMappingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Application.DTOs.Projects;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings;
+
+public static class ProjectMappingExtensions
+{
+    public static ProjectDto ToDto(this Project project)
+    {
+        return new ProjectDto
+        {
+            Id = project.Id,
+            Name = project.Name,
+            Key = project.Key,
+            Description = project.Description,
+            Status = project.Status,
+            OwnerId = project.OwnerId,
+            CreatedAt = project.CreatedAt,
+            UpdatedAt = project.UpdatedAt
+        };
+    }
+}
