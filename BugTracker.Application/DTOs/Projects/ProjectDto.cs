e655ae3489307210de30e7f1bc576da578f5b307
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:23 +0100

    feat(application): add ProjectDto

---
+++ BugTracker.Application/DTOs/Projects/ProjectDto.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Domain.Enums;
+
+namespace BugTracker.Application.DTOs.Projects;
+
+public class ProjectDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Key { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public ProjectStatus Status { get; set; }
+
+    public Guid OwnerId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
