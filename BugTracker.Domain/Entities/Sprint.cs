a176750364f07a312dbda125176dcb5f72604fd6
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:07:29 +0100

    feat(domain): update Sprint entity

--- BugTracker.Domain/Entities/Sprint.cs
+++ BugTracker.Domain/Entities/Sprint.cs
@@ -1,9 +1,4 @@
 ﻿using BugTracker.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BugTracker.Domain.Common;
 
 namespace BugTracker.Domain.Entities
