b147db5039eed778d47557631e604adf5428dd31
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:29 +0100

    feat(application): add IProjectMemberService interface

---
+++ BugTracker.Application/Interfaces/Services/IProjectMemberService.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Application.DTOs.ProjectMembers;
+using BugTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface  IProjectMemberService
+    {
+        Task<IEnumerable<ProjectMemberDto>> GetMembersAsync(Guid projectId);
+
+        Task<ProjectMemberDto> AddMemberAsync(Guid projectId,AddProjectMemberDto dto);
+
+        Task ChangeRoleAsync( Guid projectId, Guid userId, ProjectRole newRole);
+
+        Task RemoveMemberAsync(Guid projectId, Guid userId);
+    }
+}
