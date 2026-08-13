5e8cef1c047e9a429e3763863931edb7ef5638a4
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:12:24 +0100

    feat(application): add CreateAttachmentDto

---
+++ BugTracker.Application/DTOs/Attachment/CreateAttachmentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Application.DTOs.Attachments;
+
+public class CreateAttachmentDto
+{
+    public string FileName { get; set; } = string.Empty;
+
+    public string ContentType { get; set; } = string.Empty;
+
+    public Stream FileContent { get; set; } = Stream.Null;
+}
