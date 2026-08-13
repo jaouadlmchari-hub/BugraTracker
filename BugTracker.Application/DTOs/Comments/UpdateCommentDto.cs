6a2ebcbfa17fa49a25d98b1db6be500b2ccc7bab
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:12:51 +0100

    feat(application): add UpdateCommentDto

---
+++ BugTracker.Application/DTOs/Comments/UpdateCommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.DTOs.Comments
+{
+    public class UpdateCommentDto
+    {
+        public string Content { get; set; } = string.Empty;
+    }
+}
