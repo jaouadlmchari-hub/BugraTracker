4caa11ac221fcb01dbbd3fd7cdbe828616f85221
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:18 +0100

    feat(application): add ProjectMemberDto

---
+++ BugTracker.Application/DTOs/ProjectMembers/ProjectMemberDto.cs
@@ -0,0 +1,16 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.ProjectMembers
+{
+    public class ProjectMemberDto
+    {
+        public Guid UserId { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public ProjectRole Role { get; set; }
+    }
+}
