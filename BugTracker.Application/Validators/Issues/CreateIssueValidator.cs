d8ecb3deb5d7507b59e95cf78340a9c8b1fe8488
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:15:32 +0100

    feat(application): add CreateIssueValidator

---
+++ BugTracker.Application/Validators/Issues/CreateIssueValidator.cs
@@ -0,0 +1,44 @@
+﻿using BugTracker.Application.DTOs.Issues;
+using FluentValidation;
+
+
+namespace BugTracker.Application.Validators.Issues
+{
+    public class CreateIssueValidator : AbstractValidator<CreateIssueDto>
+    {
+
+        public CreateIssueValidator() 
+        {
+            RuleFor(x => x.Title)
+                  .NotEmpty()
+                  .WithMessage("Le titre du ticket est obligatoire.")
+                  .MinimumLength(5)
+                  .WithMessage("Le titre doit contenir au moins 5 caractères.")
+                  .MaximumLength(200)
+                  .WithMessage("Le titre ne peut pas dépasser 200 caractères.");
+
+
+            RuleFor(x => x.StoryPoints)
+                 .GreaterThan(0)
+                 .When(x => x.StoryPoints.HasValue)
+                 .WithMessage("Les story points doivent être strictement positifs.");
+
+            
+            RuleFor(x => x.Type)
+                 .IsInEnum()
+                 .WithMessage("Le type du ticket est invalide.");
+             
+            
+            RuleFor(x => x.Priority)
+                .IsInEnum()
+                .When(x => x.Priority.HasValue)
+                .WithMessage("La priorité du ticket est invalide.");
+
+           
+            RuleFor(x => x.DueDate)
+                .GreaterThanOrEqualTo(DateTime.UtcNow.Date)
+                .When(x => x.DueDate.HasValue)
+                .WithMessage("La date d'échéance ne peut pas être dans le passé.");
+        }
+    }
+}
