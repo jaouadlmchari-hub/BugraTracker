ecedd45a27e95001d4e3d9d55962932bb7a0dbb9
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:12:27 +0100

    feat(application): add CommentDto

---
+++ BugTracker.Application/DTOs/Comments/CommentDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BugTracker.Application.DTOs.Comments
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public Guid IssueId { get; set; }
+        public Guid AuthorId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
