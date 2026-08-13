733bb6f438e3583a2a2704a016d9281e8bd63004
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:50 +0100

    feat(application): add CommentMappingExtensions

---
+++ BugTracker.Application/Mappings/CommentMappingExtensions.cs
@@ -0,0 +1,25 @@
+﻿
+
+using BugTracker.Application.DTOs.Comments;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings
+{
+    public static  class CommentMappingExtensions
+    {
+
+        public static CommentDto ToDto(this Comment dto)
+        {
+            return new CommentDto
+            {
+                Id = dto.Id,
+                IssueId = dto.IssueId,
+                AuthorId = dto.AuthorId,
+                Content = dto.Content,
+                CreatedAt = dto.CreatedAt,
+                UpdatedAt = dto.UpdatedAt
+            };
+        }
+
+    }
+}
