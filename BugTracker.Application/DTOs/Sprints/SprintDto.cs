334010e2e275f97c4501434eec98ca54da689bda
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:51 +0100

    feat(application): add SprintDto

---
+++ BugTracker.Application/DTOs/Sprints/SprintDto.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.Sprints
+{
+    public class SprintDto
+    {
+        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string? Goal { get; set; }
+
+        public SprintStatus Status { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
