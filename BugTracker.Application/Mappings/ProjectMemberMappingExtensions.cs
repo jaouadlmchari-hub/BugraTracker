b50615f1854532c60a639fe030bd7a1ac42784de
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:56 +0100

    feat(application): add ProjectMemberMappingExtensions

---
+++ BugTracker.Application/Mappings/ProjectMemberMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Application.DTOs.ProjectMembers;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings;
+
+public static class ProjectMemberMappingExtensions
+{
+    public static ProjectMemberDto ToDto(this ProjectMember member)
+    {
+        return new ProjectMemberDto
+        {
+            UserId = member.UserId,
+            Username = member.User.Username,
+            FullName = member.User.FullName,
+            Role = member.Role
+        };
+    }
+}
