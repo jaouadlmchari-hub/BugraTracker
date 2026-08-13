205ecb251962024a84df743ab5992571ea88d739
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:12:58 +0100

    feat(application): add UpdateIssueDto

---
+++ BugTracker.Application/DTOs/Issues/UpdateIssueDto.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.Domain.Enums;
+
+namespace BugTracker.Application.DTOs.Issues;
+
+public class UpdateIssueDto
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public IssueType Type { get; set; }
+
+    public Priority Priority { get; set; }
+
+    public int? StoryPoints { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public Guid? EpicId { get; set; }
+}
