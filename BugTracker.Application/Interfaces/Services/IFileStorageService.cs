897ce4f651b508a6ecc506ddde15bfc2de5431c8
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:22 +0100

    feat(application): add IFileStorageService interface

---
+++ BugTracker.Application/Interfaces/Services/IFileStorageService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface IFileStorageService
+    {
+        Task UploadAsync(Stream fileStream,string storageFileName, string contentType);
+
+        Task<string> GenerateDownloadUrlAsync(string storageFileName, TimeSpan expiration);
+
+        Task DeleteAsync(string storageFileName);
+    }
+}
