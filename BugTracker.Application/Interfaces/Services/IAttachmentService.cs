69d999340ea05f45d33d6324661f3e263c3608c6
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:15 +0100

    feat(application): add IAttachmentService interface

---
+++ BugTracker.Application/Interfaces/Services/IAttachmentService.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Application.DTOs.Attachments;
+
+public interface IAttachmentService
+{
+    Task<AttachmentDto> UploadAsync(Guid issueId, CreateAttachmentDto dto);
+
+    Task<string> GetDownloadUrlAsync(Guid attachmentId);
+
+    Task DeleteAsync(Guid attachmentId);
+}
