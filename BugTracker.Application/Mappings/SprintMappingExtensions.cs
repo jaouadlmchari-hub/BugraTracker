2414efab081d63949672fe11ca971eb9334b48f2
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:59 +0100

    feat(application): add SprintMappingExtensions

---
+++ BugTracker.Application/Mappings/SprintMappingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BugTracker.Application.DTOs.Sprints;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings
+{
+    public static  class SprintMappingExtensions
+    {
+        public static SprintDto ToDto(this Sprint sprint)
+        {
+            return new SprintDto
+            {
+                Id = sprint.Id,
+                ProjectId = sprint.ProjectId,
+                Name = sprint.Name,
+                Goal = sprint.Goal,
+                Status = sprint.Status,
+                StartDate = sprint.StartDate,
+                EndDate = sprint.EndDate,
+                CompletedAt = sprint.CompletedAt,
+
+            };
+        }
+    }
+}
