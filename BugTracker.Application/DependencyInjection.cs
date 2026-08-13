cce6381012e5088a1fac6d8cdc180d4d3d8a4e90
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:56 +0100

    feat(application): add Application DependencyInjection configuration

---
+++ BugTracker.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using BugTracker.Application.Interfaces;
+using BugTracker.Application.Interfaces.Services;
+using BugTracker.Application.Services;
+using BugTracker.Application.Validators.Sprints;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddScoped<IProjectMemberService, ProjectMemberService>();
+        services.AddScoped<ISprintService, SprintService>();
+        services.AddScoped<IIssueService, IssueService>();
+        services.AddScoped<ICommentService, CommentService>();
+        services.AddScoped<IAttachmentService, AttachmentService>();
+
+        services.AddValidatorsFromAssemblyContaining<CreateSprintValidator>();
+
+        return services;
+    }
+}
