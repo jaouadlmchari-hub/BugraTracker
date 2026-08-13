6819c8a824a69889939c58e86ddbab4c0d85dfed
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:48 +0100

    feat(application): add AttachmentMappingExtensions

---
+++ BugTracker.Application/Mappings/AttachmentMappingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings
+{
+    public static class AttachmentMappingExtensions
+    {
+        public static AttachmentDto ToDto(this Attachment attachment, string downloadUrl)
+        {
+            return new AttachmentDto
+            {
+                Id = attachment.Id,
+                IssueId = attachment.IssueId,
+                UploaderId = attachment.UploaderId,
+                Filename = attachment.Filename,
+                MimeType = attachment.MimeType,
+                SizeBytes = attachment.SizeBytes,
+                DownloadUrl = downloadUrl,
+                CreatedAt = attachment.CreatedAt
+            };
+        }
+    }
+}
