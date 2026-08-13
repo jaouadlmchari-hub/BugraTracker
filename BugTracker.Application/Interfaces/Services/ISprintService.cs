df1bfda5d60970fcffb93cce06cc6740491b4c35
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:45 +0100

    feat(application): add ISprintService interface

---
+++ BugTracker.Application/Interfaces/Services/ISprintService.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Application.DTOs.Sprints;
+
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface ISprintService
+    {
+        Task<SprintDto?> GetByIdAsync(Guid sprintId);
+
+        Task<IEnumerable<SprintDto>> GetAllByProjectAsync(Guid projetId);
+
+        Task<SprintDto> CreateAsync(Guid projetId , CreateSprintDto dto);
+
+        Task<SprintDto> UpdateAsync(Guid sprintId , UpdateSprintDto dto);
+
+        Task StartAsync(Guid sprintId);
+
+        Task CompleteAsync(Guid sprintId);
+
+        Task DeleteAsync(Guid sprintId);
+
+    }
+}
