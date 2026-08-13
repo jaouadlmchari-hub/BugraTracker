bfbe4292a54f7e63d1c6abfc155f3ec819f772a1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:14:17 +0100

    feat(application): add ICommentService interface

---
+++ BugTracker.Application/Interfaces/Services/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using BugTracker.Application.DTOs.Comments;
+
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface ICommentService 
+    {
+        Task<CommentDto> CreateAsync(Guid issueId, CreateCommentDto dto);
+
+        Task<CommentDto> UpdateAsync(Guid commentId, UpdateCommentDto dto);
+
+        Task DeleteAsync(Guid commentId);
+    }
+}
