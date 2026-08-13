091f871259c59521637bc36bf7672a6df6f8ab4a
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:15:41 +0100

    feat(infrastructure): add S3Settings configuration

---
+++ BugTracker.Infrastructure/Configurations/S3Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Infrastructure.Configurations;
+
+public class S3Settings
+{
+    public string ServiceUrl { get; set; } = string.Empty;
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = string.Empty;
+    public string Region { get; set; } = string.Empty;
+}
