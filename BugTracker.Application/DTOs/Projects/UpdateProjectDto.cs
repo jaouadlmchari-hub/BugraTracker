c7e5f4614d7c7b04f430d62de53a61ddddff29e1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-13 05:13:47 +0100

    feat(application): add UpdateProjectDto

---
+++ BugTracker.Application/DTOs/Projects/UpdateProjectDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Application.DTOs.Projects;
+
+public class UpdateProjectDto
+{
+    [Required(ErrorMessage = "Le nom du projet est obligatoire.")]
+    [StringLength(100, ErrorMessage = "Le nom du projet ne peut pas dépasser 100 caractères.")]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(500, ErrorMessage = "La description ne peut pas dépasser 500 caractères.")]
+    public string? Description { get; set; }
+}
