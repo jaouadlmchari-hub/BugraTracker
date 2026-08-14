dcbbe6d6c12d085138b983b4f23f208a88b3dcc5
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:04 +0100

    feat(application): add IRefreshTokenService interface

---
+++ BugTracker.Application/Interfaces/Services/IRefreshTokenService.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Domain.Entities;
+
+public interface IRefreshTokenService
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+
+    Task<IEnumerable<RefreshToken>> GetByUserIdAsync(Guid userId);
+
+    Task RevokeAsync(Guid tokenId);
+}
