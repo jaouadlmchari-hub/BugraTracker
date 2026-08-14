7457da7186baa1c86b19e4508b12548d4f49a47b
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:00 +0100

    feat(application): add EpicDto

---
+++ BugTracker.Application/DTOs/Epics/EpicDto.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.Epics
+{
+    public class EpicDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public string ColorCode { get; set; } = "#3B82F6";
+
+        public EpicStatus Status { get; set; }
+    }
+}
