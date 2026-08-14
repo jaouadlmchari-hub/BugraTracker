85305591b198bfc93fd477fc31ecef761d49c674
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:03 +0100

    feat(application): add IEpicService interface

---
+++ BugTracker.Application/Interfaces/Services/IEpicService.cs
@@ -0,0 +1,26 @@
+﻿using BugTracker.Application.DTOs.Epics;
+using BugTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface IEpicService
+    {
+        Task<EpicDto> CreateAsync(Guid projectId, CreateEpicDto dto);
+
+        Task<EpicDto> UpdateAsync(Guid epicId, UpdateEpicDto dto);
+
+        Task<EpicDto?> GetByIdAsync(Guid epicId);
+
+        Task<EpicDetailsDto?> GetByIdWithDetailsAsync(Guid epicId);
+
+        Task DeleteAsync(Guid epicId);
+
+        Task ChangeStatusAsync(Guid epicId, EpicStatus newStatus);
+
+    }
+}
