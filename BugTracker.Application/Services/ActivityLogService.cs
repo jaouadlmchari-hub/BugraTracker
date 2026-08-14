6dcf6d3cbb95dd30e16a2bc1789906bcec01d66c
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:06 +0100

    feat(application): add ActivityLogService implementation

---
+++ BugTracker.Application/Services/ActivityLogService.cs
@@ -0,0 +1,36 @@
+﻿using BugTracker.Application.Interfaces;
+using BugTracker.Application.Interfaces.Repositories;
+using BugTracker.Domain.Entities;
+using BugTracker.Domain.Enums;
+
+public class ActivityLogService : IActivityLogService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ActivityLogService(
+        IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task LogAsync(
+        Guid issueId,
+        Guid userId,
+        ActivityAction action,
+        string? field = null,
+        string? fromValue = null,
+        string? toValue = null)
+    {
+        var activityLog = new ActivityLog
+        {
+            IssueId = issueId,
+            UserId = userId,
+            Action = action,
+            Field = field,
+            FromValue = fromValue,
+            ToValue = toValue
+        };
+
+        await _unitOfWork.ActivityLogs.AddAsync(activityLog);
+    }
+}
