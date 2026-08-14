50c27f165b794ed9baa2f0ec8a932979350e526b
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:38:58 +0100

    feat(application): add CreateEpicDto

---
+++ BugTracker.Application/DTOs/Epics/CreateEpicDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BugTracker.Application.DTOs.Epics
+{
+    public class CreateEpicDto
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public string ColorCode { get; set; } = "#3B82F6";
+    }
+}
