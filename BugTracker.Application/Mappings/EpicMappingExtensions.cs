f26995711c095f7fee152c87582836c850ea0959
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:05 +0100

    feat(application): add EpicMappingExtensions

---
+++ BugTracker.Application/Mappings/EpicMappingExtensions.cs
@@ -0,0 +1,39 @@
+﻿using BugTracker.Application.DTOs.Epics;
+using BugTracker.Domain.Entities;
+
+
+namespace BugTracker.Application.Mappings
+{
+    public static  class EpicMappingExtensions
+    {
+        public static EpicDto ToDto(this Epic epic)
+        {
+            return new EpicDto
+            {
+                Id = epic.Id,
+                ProjectId = epic.ProjectId,
+                Title = epic.Title,
+                Description = epic.Description,
+                ColorCode = epic.ColorCode,
+                Status = epic.Status,
+            };
+        }
+
+        public static EpicDetailsDto ToDetailsDto(this Epic epic)
+        {
+            return new EpicDetailsDto
+            {
+                Id = epic.Id,
+                ProjectId = epic.ProjectId,
+                Title = epic.Title,
+                Description = epic.Description,
+                ColorCode = epic.ColorCode,
+                Status = epic.Status,
+
+                Issues = epic.Issues
+                    .Select(i => i.ToDto())
+                    .ToList()
+            };
+        }
+    }
+}
