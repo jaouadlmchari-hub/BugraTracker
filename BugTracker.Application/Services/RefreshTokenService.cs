e5f1f072277a1e8272d8f5a560aa5159aef129db
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:08 +0100

    feat(application): add RefreshTokenService implementation

---
+++ BugTracker.Application/Services/RefreshTokenService.cs
@@ -0,0 +1,32 @@
+﻿using BugTracker.Application.Interfaces;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Services;
+
+public class RefreshTokenService : IRefreshTokenService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RefreshTokenService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        return await _unitOfWork.RefreshTokens
+            .GetByTokenAsync(token);
+    }
+
+    public async Task<IEnumerable<RefreshToken>> GetByUserIdAsync(Guid userId)
+    {
+        return await _unitOfWork.RefreshTokens
+            .GetByUserIdAsync(userId);
+    }
+
+    public async Task RevokeAsync(Guid tokenId)
+    {
+        await _unitOfWork.RefreshTokens
+            .RevokeAsync(tokenId);
+    }
+}
