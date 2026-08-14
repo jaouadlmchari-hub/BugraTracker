7a3c071a59a4a3b1bf2604cd694ce55eabcc0b52
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-14 04:39:02 +0100

    feat(application): add IActivityLogService interface

---
+++ BugTracker.Application/Interfaces/Services/IActivityLogService.cs
@@ -0,0 +1,12 @@
+﻿using BugTracker.Domain.Enums;
+
+public interface IActivityLogService
+{
+    Task LogAsync(
+        Guid issueId,
+        Guid userId,
+        ActivityAction action,
+        string? field = null,
+        string? fromValue = null,
+        string? toValue = null);
+}
