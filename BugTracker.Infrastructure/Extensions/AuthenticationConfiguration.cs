7ff73d8b4e78e58755d3c5050d5486c722744042
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:59:16 +0100

    feat(infrastructure): add AuthenticationConfiguration extensions

---
+++ BugTracker.Infrastructure/Extensions/AuthenticationConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using BugTracker.Application.Interfaces.Services;
+using BugTracker.Infrastructure.Configurations;
+using BugTracker.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Infrastructure.Extensions;
+
+public static class AuthenticationConfiguration
+{
+    public static IServiceCollection AddAuthenticationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // 1. Charger la section "Jwt" dans JwtSettings
+        services.Configure<JwtSettings>(
+            configuration.GetSection("Jwt"));
+
+        // 1. Charger la section "RefreshToken" dans RefreshTokenSettings
+        services.Configure<RefreshTokenSettings>(
+            configuration.GetSection("RefreshToken"));
+
+        // 3. Enregistrer l'implémentation de ITokenService
+        services.AddScoped<ITokenService, JwtTokenService>();
+
+        // 4. Enregistrer l'implémentation de IRefreshTokenGenerator
+        services.AddScoped<IRefreshTokenGenerator, RefreshTokenGenerator>();
+
+
+
+        return services;
+    }
+}
