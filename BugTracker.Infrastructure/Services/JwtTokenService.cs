244bffabb8414018b8928036920d49a3466f1b77
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:59:18 +0100

    feat(infrastructure): add JwtTokenService implementation

---
+++ BugTracker.Infrastructure/Services/JwtTokenService.cs
@@ -0,0 +1,65 @@
+﻿using BugTracker.Application.Interfaces.Services;
+using BugTracker.Application.Models.Auth;
+using BugTracker.Domain.Entities;
+using BugTracker.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BugTracker.Infrastructure.Services;
+
+public class JwtTokenService : ITokenService
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenService(IOptions<JwtSettings> options)
+    {
+        _jwtSettings = options.Value;
+    }
+
+    public AccessTokenResult GenerateAccessToken(User user)
+    {
+        var expiresAt = DateTime.UtcNow.AddMinutes(_jwtSettings.AccessTokenExpirationMinutes);
+
+        // 1. Claims
+        var claims = new List<Claim>
+        {
+            new(
+                ClaimTypes.NameIdentifier,
+                user.Id.ToString()),
+
+            new(
+                ClaimTypes.Email,
+                user.Email),
+
+            new(
+                ClaimTypes.Role,
+                user.SystemRole.ToString())
+        };
+
+        // 2. Transformer la SecretKey en bytes
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(
+                _jwtSettings.SecretKey));
+
+        // 3. Créer la signature
+        var credentials = new SigningCredentials(
+            key,
+            SecurityAlgorithms.HmacSha256);
+
+        // 4. Construire le JWT
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: expiresAt,
+            signingCredentials: credentials);
+
+        // 5. Transformer l'objet JWT en string
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return new AccessTokenResult(tokenValue,expiresAt);
+    }
+}
