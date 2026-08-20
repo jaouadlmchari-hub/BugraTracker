dac6762aa642f3bc9224f3383f4b5a1dcdec3717
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:59:07 +0100

    feat(application): add IRefreshTokenGenerator interface

---
+++ BugTracker.Application/Interfaces/Services/IRefreshTokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Application.Models.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Interfaces.Services
+{
+    public interface IRefreshTokenGenerator
+    {
+        RefreshTokenResult Generate();
+    }
+}
