42feec3ee07f38134f0fc06a8013557e1d46c1a4
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:59:19 +0100

    feat(infrastructure): add RefreshTokenGenerator implementation

---
+++ BugTracker.Infrastructure/Services/RefreshTokenGenerator.cs
@@ -0,0 +1,33 @@
+﻿using BugTracker.Application.Interfaces.Services;
+using BugTracker.Application.Models.Auth;
+using BugTracker.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+
+namespace BugTracker.Infrastructure.Services;
+
+public class RefreshTokenGenerator : IRefreshTokenGenerator
+{
+    private readonly RefreshTokenSettings _settings;
+
+    public RefreshTokenGenerator(IOptions<RefreshTokenSettings> options)
+    {
+        _settings = options.Value;
+    }
+
+    public RefreshTokenResult Generate()
+    {
+        var randomBytes =
+            RandomNumberGenerator.GetBytes(64);
+
+        var token =
+            Convert.ToBase64String(randomBytes);
+
+        var expiresAt =
+            DateTime.UtcNow.AddDays(_settings.ExpirationDays);
+
+        return new RefreshTokenResult(
+            token,
+            expiresAt);
+    }
+}
