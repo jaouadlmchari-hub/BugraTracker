6dfc4672bb28a6ed07b1d0541095bdb5bc674773
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:58:57 +0100

    feat(application): add AuthResponseDto

---
+++ BugTracker.Application/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Application.DTOs.Auth;
+
+public class AuthResponseDto
+{
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+
+    public DateTime ExpiresAt { get; set; }
+}
