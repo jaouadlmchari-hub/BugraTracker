cd2bf5dc7fcf4df416a23f69380d16a636d07b61
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-20 05:59:06 +0100

    feat(application): add IAuthService interface

---
+++ BugTracker.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using BugTracker.Application.DTOs.Auth;
+
+namespace BugTracker.Application.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> LoginAsync(LoginDto dto);
+}
