0c65651680fd6d0e6d944a53fba62f76cb665b91
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-23 17:47:44 +0100

    feat(api): add CurrentUserService implementation

---
+++ BugTracker.API/Services/CurrentUserService.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using BugTracker.Application.Interfaces.Services;
+
+namespace BugTracker.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool IsAuthenticated =>
+            _httpContextAccessor.HttpContext?
+                .User?
+                .Identity?
+                .IsAuthenticated == true;
+
+        public Guid UserId
+        {
+            get
+            {
+                var userIdClaim =
+                    _httpContextAccessor.HttpContext?
+                        .User?
+                        .FindFirst(ClaimTypes.NameIdentifier)?
+                        .Value;
+
+                if (Guid.TryParse(userIdClaim, out var userId))
+                {
+                    return userId;
+                }
+
+                return Guid.Empty;
+            }
+        }
+
+        public bool IsAdmin =>
+            _httpContextAccessor.HttpContext?
+                .User?
+                .IsInRole("Admin") == true;
+    }
+}
