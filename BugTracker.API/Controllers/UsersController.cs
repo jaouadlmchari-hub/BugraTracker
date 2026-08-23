1b0fca77ecae296549cafe74723763bfd9cbb754
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-23 17:47:40 +0100

    feat(api): add UsersController

---
+++ BugTracker.API/Controllers/UsersController.cs
@@ -0,0 +1,256 @@
+﻿using BugTracker.Application.DTOs.Common;
+using BugTracker.Application.DTOs.Users;
+using BugTracker.Application.Interfaces.Services;
+using BugTracker.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IAuthorizationService _authorizationService;
+
+        public UsersController(
+            IUserService userService,
+            ICurrentUserService currentUserService,
+            IAuthorizationService authorizationService)
+        {
+            _userService = userService;
+            _currentUserService = currentUserService;
+            _authorizationService = authorizationService;
+        }
+
+
+        [HttpGet("me")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userService.GetByIdAsync( _currentUserService.UserId);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<UserDto>> GetById(Guid id)
+        {
+            var authorizationResult =
+                await _authorizationService.AuthorizeAsync(
+                    User,
+                    id,
+                    "CanViewUser");
+
+            if (!authorizationResult.Succeeded)
+                return Forbid();
+
+            var user = await _userService.GetByIdAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpGet("active")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(IEnumerable<UserDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetActiveUsers()
+        {
+            var users = await _userService.GetActiveUsersAsync();
+
+            return Ok(users);
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(PagedResultDto<UserDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<PagedResultDto<UserDto>>> GetAll([FromQuery] UserFilterDto filter)
+        {
+            var result =
+                await _userService.GetAllPaginatedAsync(filter);
+
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<UserDto>> Create(CreateUserDto dto)
+        {
+            var user = await _userService.CreateAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = user.Id },
+                user);
+        }
+
+
+        [HttpPost("admin")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<UserDto>> AdminCreate(AdminCreateUserDto dto)
+        {
+            var user = await _userService.AdminCreateAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = user.Id },
+                user);
+        }
+
+
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<UserDto>> Update(Guid id, UpdateUserDto dto)
+        {
+
+            if (!_currentUserService.IsAdmin &&
+                        _currentUserService.UserId != id)
+            {
+                return Forbid();
+            }
+            var user = await _userService.UpdateAsync(id, dto);
+
+            return Ok(user);
+        }
+
+
+
+        [HttpPatch("{id:guid}/deactivate")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> Deactivate(Guid id)
+        {
+            await _userService.DeactivateAsync(id);
+
+            return NoContent();
+        }
+
+
+
+        [HttpPatch("{id:guid}/activate")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> Activate(Guid id)
+        {
+            await _userService.ActivateAsync(id);
+
+            return NoContent();
+        }
+
+
+
+        [HttpPatch("{id:guid}/role")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> ChangeSystemRole(Guid id , [FromBody] SystemRole newRole)
+        {
+            await _userService.ChangeSystemRoleAsync(id, newRole);
+
+            return NoContent();
+        }
+
+
+
+        [HttpPost("{id:guid}/change-password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> ChangePassword(Guid id , ChangePasswordDto dto)
+        {
+            if (_currentUserService.UserId != id)
+            {
+                return Forbid();
+            }
+
+            await _userService.ChangePasswordAsync(id, dto);
+
+            return NoContent();
+        }
+
+
+
+
+        [HttpPost("{id:guid}/reset-password")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> ResetPassword(Guid id , ResetPasswordDto dto)
+        {
+            await _userService.ResetPasswordAsync(id, dto);
+
+            return NoContent();
+        }
+
+
+
+
+        [HttpPatch("{id:guid}/unlock")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> Unlock(Guid id)
+        {
+            await _userService.UnlockUserAsync(id);
+
+            return NoContent();
+        }
+    }
+}
