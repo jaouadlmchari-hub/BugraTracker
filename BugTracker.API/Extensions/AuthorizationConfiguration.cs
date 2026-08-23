d3847db636456a1f16b5bc96268203f83f73842b
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-23 17:47:42 +0100

    feat(api): add AuthorizationConfiguration extension

---
+++ BugTracker.API/Extensions/AuthorizationConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.API.Authorization.Handlers;
+using BugTracker.API.Authorization.Requirements;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BugTracker.API.Extensions;
+
+public static class AuthorizationConfiguration
+{
+    public static IServiceCollection AddAuthorizationPolicies(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(
+                "CanViewUser",
+                policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+
+                    policy.AddRequirements(
+                        new CanViewUserRequirement());
+                });
+        });
+
+        services.AddScoped<IAuthorizationHandler, CanViewUserHandler>();
+
+        return services;
+    }
+}
