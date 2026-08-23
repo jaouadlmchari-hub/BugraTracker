0c5ea41959ff726eaf31e77e4248c6bb8745454c
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-23 17:47:38 +0100

    feat(api): add AuthController

---
+++ BugTracker.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using BugTracker.Application.DTOs.Auth;
+using BugTracker.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(
+            LoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            return Ok(result);
+        }
+
+
+
+      
+    }
+}
