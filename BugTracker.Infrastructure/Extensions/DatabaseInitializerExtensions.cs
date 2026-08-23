a25d8fe586e2b06a6d1705550771624e8b3d4f22
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-23 17:47:48 +0100

    feat(infrastructure): add DatabaseInitializerExtensions

---
+++ BugTracker.Infrastructure/Extensions/DatabaseInitializerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Infrastructure.Persistence.Seed;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Infrastructure.Extensions;
+
+public static class DatabaseInitializerExtensions
+{
+    public static async Task UseDatabaseSeederAsync(
+        this IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+
+        var seeder = scope.ServiceProvider
+            .GetRequiredService<DatabaseSeeder>();
+
+        await seeder.SeedAsync();
+    }
+}
