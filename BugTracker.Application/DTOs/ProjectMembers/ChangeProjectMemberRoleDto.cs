16b7eae76e8b30f4c6c978b5a46df2521a3aaab1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-24 19:33:54 +0100

    feat(application): add ChangeProjectMemberRoleDto

---
+++ BugTracker.Application/DTOs/ProjectMembers/ChangeProjectMemberRoleDto.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.ProjectMembers
+{
+    public class ChangeProjectMemberRoleDto
+    {
+        public ProjectRole NewRole { get; set; }
+    }
+}
