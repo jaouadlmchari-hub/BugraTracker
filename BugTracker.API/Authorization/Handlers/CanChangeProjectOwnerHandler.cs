a35f4d17da7a6040884e8abefff98349174cef95
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-24 05:43:43 +0100

    feat(api): add CanChangeProjectOwnerHandler authorization handler

---
+++ BugTracker.API/Authorization/Handlers/CanChangeProjectOwnerHandler.cs
@@ -0,0 +1,39 @@
+﻿using BugTracker.API.Authorization.Requirements;
+using BugTracker.Application.DTOs.Projects;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BugTracker.API.Authorization.Handlers;
+
+public class CanChangeProjectOwnerHandler
+    : AuthorizationHandler<CanChangeProjectOwnerRequirement, ProjectDto>
+{
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        CanChangeProjectOwnerRequirement requirement,
+        ProjectDto project)
+    {
+      
+        if (context.User.IsInRole("Admin"))
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+
+       
+        var userIdClaim = context.User
+            .FindFirst(ClaimTypes.NameIdentifier)?
+            .Value;
+
+        if (!Guid.TryParse(userIdClaim, out var currentUserId))
+            return Task.CompletedTask;
+
+     
+        if (currentUserId == project.OwnerId)
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
