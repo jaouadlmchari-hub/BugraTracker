7a3a12f6b3b8745c2839bebe04c431979276ec48
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-24 05:43:45 +0100

    feat(api): add CanManageProjectHandler authorization handler

---
+++ BugTracker.API/Authorization/Handlers/CanManageProjectHandler.cs
@@ -0,0 +1,49 @@
+﻿using BugTracker.API.Authorization.Requirements;
+using BugTracker.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BugTracker.API.Authorization.Handlers;
+
+public class CanManageProjectHandler : AuthorizationHandler<CanManageProjectRequirement, Guid>
+{
+    private readonly IProjectMemberService _projectMemberService;
+
+    public CanManageProjectHandler(
+        IProjectMemberService projectMemberService)
+    {
+        _projectMemberService = projectMemberService;
+    }
+
+    protected override async Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        CanManageProjectRequirement requirement,
+        Guid projectId)
+    {
+       
+        if (context.User.IsInRole("Admin"))
+        {
+            context.Succeed(requirement);
+            return;
+        }
+
+     
+        var userIdClaim = context.User
+            .FindFirst(ClaimTypes.NameIdentifier)?
+            .Value;
+
+        if (!Guid.TryParse(userIdClaim, out var currentUserId))
+            return;
+
+        // Vérifier son rôle dans CE projet
+        var isManager =
+            await _projectMemberService.IsManagerAsync(
+                projectId,
+                currentUserId);
+
+        if (isManager)
+        {
+            context.Succeed(requirement);
+        }
+    }
+}
