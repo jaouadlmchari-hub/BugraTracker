a719c47cc7d069c76e5af15174f7f98163ac6310
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-25 02:38:13 +0100

    feat(application): update Application DependencyInjection configuration

--- BugTracker.Application/DependencyInjection.cs
+++ BugTracker.Application/DependencyInjection.cs
@@ -21,6 +21,7 @@ public static class DependencyInjection
         services.AddScoped<IAttachmentService, AttachmentService>();
         services.AddScoped<IActivityLogService, ActivityLogService>();
         services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+        services.AddScoped<IEpicService, EpicService>();
 
         services.AddValidatorsFromAssemblyContaining<CreateSprintValidator>();
 
