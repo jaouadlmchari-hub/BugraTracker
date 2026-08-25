2f2688649b1b984556549cb027c1a553c8c671dc
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-25 02:38:43 +0100

    feat(application): add ChangeEpicStatusDto

---
+++ BugTracker.Application/DTOs/Epics/ChangeEpicStatusDto.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.Epics
+{
+    public class ChangeEpicStatusDto
+    {
+        public EpicStatus NewStatus { get; set; }
+    }
+}
