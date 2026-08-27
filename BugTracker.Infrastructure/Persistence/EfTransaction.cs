4d1e684710abf795a3ad298ff2aec9019da0c076
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:40:10 +0100

    feat(infrastructure): add EfTransaction implementation

---
+++ BugTracker.Infrastructure/Persistence/EfTransaction.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Application.Interfaces.Persistence;
+using Microsoft.EntityFrameworkCore.Storage;
+
+public class EfTransaction : ITransaction
+{
+    private readonly IDbContextTransaction _transaction;
+
+    public EfTransaction(IDbContextTransaction transaction)
+    {
+        _transaction = transaction;
+    }
+
+   public Task CommitAsync()
+        => _transaction.CommitAsync();
+
+    public Task RollbackAsync()
+        => _transaction.RollbackAsync();
+
+    public ValueTask DisposeAsync()
+        => _transaction.DisposeAsync();
+}
