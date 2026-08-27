ede76f3c3161c2a770d84478f10e81869c185871
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:40:05 +0100

    feat(application): add IUnitOfWork interface

---
+++ BugTracker.Application/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using BugTracker.Application.Interfaces.Repositories;
+
+
+namespace BugTracker.Application.Interfaces.Persistence
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+        IProjectRepository Projects { get; }
+        IProjectMemberRepository ProjectMembers { get; }
+        ISprintRepository Sprints { get; }
+        IEpicRepository Epics { get; }
+        IIssueRepository Issues { get; }
+        ICommentRepository Comments { get; }
+        IAttachmentRepository Attachments { get; }
+        IActivityLogRepository ActivityLogs { get; }
+        IRefreshTokenRepository RefreshTokens { get; }
+
+        Task<ITransaction> BeginTransactionAsync();
+
+        Task<int> SaveChangesAsync();
+    }
+}
