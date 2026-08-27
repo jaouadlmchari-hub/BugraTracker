aa2ea00339225af5ba036ab3d70eb6f9772e8bb4
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:39:42 +0100

    feat(application): add ActivityLogDto

---
+++ BugTracker.Application/DTOs/ActivityLogs/ActivityLogDto.cs
@@ -0,0 +1,24 @@
+﻿using BugTracker.Domain.Enums;
+
+namespace BugTracker.Application.DTOs.ActivityLogs;
+
+public class ActivityLogDto
+{
+    public Guid Id { get; set; }
+
+    public Guid IssueId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public ActivityAction Action { get; set; }
+
+    public string? Field { get; set; }
+
+    public string? FromValue { get; set; }
+
+    public string? ToValue { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
