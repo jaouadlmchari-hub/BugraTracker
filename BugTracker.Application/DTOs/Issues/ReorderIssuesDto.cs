a7a51e4e087507ebea09e1634131a711bef9be20
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:40:00 +0100

    feat(application): add ReorderIssuesDto

---
+++ BugTracker.Application/DTOs/Issues/ReorderIssuesDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BugTracker.Application.DTOs.Issues
+{
+    public class ReorderIssuesDto
+    {
+        [MinLength(1)]
+        public List<ReorderIssueItemDto> Items { get; set; } = new();
+    }
+}
