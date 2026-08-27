46b0618ebd2290d11bdaca72b2870a464d07ab4c
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:39:46 +0100

    feat(application): add ChangeIssueStatusDto

---
+++ BugTracker.Application/DTOs/Issues/ChangeIssueStatusDto.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Domain.Enums;
+
+
+namespace BugTracker.Application.DTOs.Issues
+{
+    public class ChangeIssueStatusDto
+    {
+        public IssueStatus NewStatus { get; set; }
+    }
+}
