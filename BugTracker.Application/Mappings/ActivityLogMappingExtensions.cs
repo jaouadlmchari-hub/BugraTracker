0d3584b6cad5919dbf5aca851fbf17e9f6ebb8a2
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:40:07 +0100

    feat(application): add ActivityLogMappingExtensions

---
+++ BugTracker.Application/Mappings/ActivityLogMappingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BugTracker.Application.DTOs.ActivityLogs;
+using BugTracker.Domain.Entities;
+
+
+namespace BugTracker.Application.Mappings
+{
+    public static class ActivityLogMappingExtensions
+    {
+        public static ActivityLogDto ToDto(this ActivityLog log)
+        {
+            return new ActivityLogDto
+            {
+                Id = log.Id,
+                IssueId = log.IssueId,
+                UserId = log.UserId,
+                UserName = log.User?.Username,
+                Action = log.Action,
+                Field = log.Field,
+                FromValue = log.FromValue,
+                ToValue = log.ToValue,
+                CreatedAt = log.CreatedAt
+            };
+        }
+    }
+}
