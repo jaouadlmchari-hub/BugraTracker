51a48bdc446838e69a2f0a756fc5d98eb544601f
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:39:17 +0100

    feat(api): add CanEditCommentHandler authorization handler

---
+++ BugTracker.API/Authorization/Handlers/CanEditCommentHandler.cs
@@ -0,0 +1,31 @@
+﻿using BugTracker.API.Authorization.Requirements;
+using BugTracker.Application.DTOs.Comments;
+using BugTracker.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BugTracker.API.Authorization.Handlers
+{
+    public class CanEditCommentHandler: AuthorizationHandler<CanEditCommentRequirement, CommentDto>
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public CanEditCommentHandler(
+            ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            CanEditCommentRequirement requirement,
+            CommentDto comment)
+        {
+            if (comment.AuthorId == _currentUserService.UserId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
