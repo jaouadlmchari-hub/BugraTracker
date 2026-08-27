2df52ded31f13de52431c9bab1af9e546c548bff
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:39:49 +0100

    feat(application): add ChangeStoryPointsDto

---
+++ BugTracker.Application/DTOs/Issues/ChangeStoryPointsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.DTOs.Issues
+{
+    public class ChangeStoryPointsDto
+    {
+        [Range(0, 100, ErrorMessage = "Les Story Points doivent être compris entre 0 et 100.")]
+        public int? StoryPoints { get; set; }
+    }
+}
