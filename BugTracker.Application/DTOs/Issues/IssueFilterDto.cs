79d3001c15b04f57b46119754598364299f59fea
jaouad El m'chari <jawadlmchari@gmail.com>
2026-08-27 04:39:51 +0100

    feat(application): add IssueFilterDto

---
+++ BugTracker.Application/DTOs/Issues/IssueFilterDto.cs
@@ -0,0 +1,26 @@
+﻿using BugTracker.Domain.Enums;
+
+public class IssueFilterDto
+{
+    public string? Search { get; set; }
+
+    public IssueStatus? Status { get; set; }
+
+    public IssueType? Type { get; set; }
+
+    public Priority? Priority { get; set; }
+
+    public Guid? AssigneeId { get; set; }
+
+    public Guid? ReporterId { get; set; }
+
+    public Guid? SprintId { get; set; }
+
+    public Guid? EpicId { get; set; }
+
+    public bool? BacklogOnly { get; set; }
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 20;
+}
